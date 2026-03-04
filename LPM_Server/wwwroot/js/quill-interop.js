429d59290b287eae5396adc5438adfb631cc715a
Yaniv Berliner <yanivberliner@gmail.com>
2026-03-04 23:14:17 +0200

    OnHoover

--- LPM_Server/wwwroot/js/quill-interop.js
+++ LPM_Server/wwwroot/js/quill-interop.js
@@ -33,6 +33,30 @@ window.quillInterop = (function () {
             dotNetRef.invokeMethodAsync('OnContentChanged', isEmpty ? '' : html);
         });
 
+        // Add tooltips to toolbar buttons
+        const toolbar = quill.container.previousSibling;
+        const tips = {
+            'ql-bold':                        'Bold',
+            'ql-italic':                      'Italic',
+            'ql-underline':                   'Underline',
+            'ql-strike':                      'Strikethrough',
+            'ql-clean':                       'Remove formatting',
+        };
+        Object.entries(tips).forEach(([cls, label]) => {
+            const el = toolbar.querySelector('.' + cls);
+            if (el) el.title = label;
+        });
+        const orderedBtn = toolbar.querySelector('.ql-list[value="ordered"]');
+        if (orderedBtn) orderedBtn.title = 'Numbered list';
+        const bulletBtn = toolbar.querySelector('.ql-list[value="bullet"]');
+        if (bulletBtn) bulletBtn.title = 'Bullet list';
+        const colorPicker = toolbar.querySelector('.ql-color .ql-picker-label');
+        if (colorPicker) colorPicker.title = 'Text color';
+        const bgPicker = toolbar.querySelector('.ql-background .ql-picker-label');
+        if (bgPicker) bgPicker.title = 'Highlight color';
+        const sizePicker = toolbar.querySelector('.ql-size .ql-picker-label');
+        if (sizePicker) sizePicker.title = 'Text size';
+
         instances.set(editorEl, quill);
     }
 
